495719a401eba4f1a700c63318d95793cf8b1517
sarabastian <seb118@georgetown.edu>
2021-04-12 10:46:18 -0400

    added gatsby node file

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,18 +12,3 @@ module.exports = {
     },
   ],
 };
-
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /offending-module/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    });
-  }
-};
