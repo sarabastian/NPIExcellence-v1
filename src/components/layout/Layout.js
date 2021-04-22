ece60ae985ad22a2b8b9bb424a81779f733ce8b0
sarabastian <seb118@georgetown.edu>
2021-04-21 22:04:32 -0400

    seo

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -11,11 +11,13 @@ const Layout = ({ children }) => {
         <Helmet>
           <html lang="en" />
           <title>NPI Excellence</title>
-          <description>
-            New product introductions are the life blood of a fast growth, high margin business. NPI
-            Excellence, LLC can enable your company to beat the competition and delight customers,
-            achieving your business objectives.
-          </description>
+          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta
+            name="description"
+            content="New product introductions are the life blood of a fast growth, high margin business. NPI
+  Excellence can enable your company to beat the competition and delight customers,
+  achieving your business objectives"
+          />
         </Helmet>
         {children}
       </main>
